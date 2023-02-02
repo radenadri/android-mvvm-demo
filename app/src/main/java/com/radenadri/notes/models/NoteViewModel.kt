b07c9c9fb13fe08452e390df7a1a17f1c008053d
Adriana Eka Prayudha <radenadriep@gmail.com>
2023-02-02 23:05:56 +0700

    wip

--- app/src/main/java/com/radenadri/notes/models/NoteViewModel.kt
+++ app/src/main/java/com/radenadri/notes/models/NoteViewModel.kt
@@ -3,7 +3,6 @@ package com.radenadri.notes.models
 import android.app.Application
 import androidx.lifecycle.AndroidViewModel
 import androidx.lifecycle.LiveData
-import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.viewModelScope
 import com.radenadri.notes.database.NoteDatabase
 import com.radenadri.notes.database.NotesRepository
@@ -17,32 +16,32 @@ class NoteViewModel(application: Application) : AndroidViewModel(application) {
     val allNotes : LiveData<List<Note>>
 
     init {
-        val dao = NoteDatabase.getDatabase(application).getNoteDao()
-        repository = NotesRepository(dao)
-        allNotes = repository.allNotes
-
-        /* val userData = fillData()
+        // Fill the database with dummy data
+        /* val userData = List<Note> {
+            return listOf<Note>(
+                Note(
+                    1,
+                    "Title 1",
+                    "Content 1",
+                    "Date 1"
+                ),
+                Note(
+                    2,
+                    "Title 2",
+                    "Content 2",
+                    "Date 2"
+                ),
+            )
+        }
         val data = MutableLiveData<List<Note>>()
         data.value = userData
         allNotes = data */
-    }
 
-    /* fun fillData() : List<Note> {
-        return listOf<Note>(
-            Note(
-                1,
-                "Title 1",
-                "Content 1",
-                "Date 1"
-            ),
-            Note(
-                2,
-                "Title 2",
-                "Content 2",
-                "Date 2"
-            ),
-        )
-    } */
+        // Get the database and the dao
+        val dao = NoteDatabase.getDatabase(application).getNoteDao()
+        repository = NotesRepository(dao)
+        allNotes = repository.allNotes
+    }
 
     fun deleteNote(note: Note) = viewModelScope.launch(Dispatchers.IO) {
         repository.delete(note)
